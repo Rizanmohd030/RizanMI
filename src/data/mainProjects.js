383ab43c3007e346a6bdbf139e050eee6e113b97
Rizan <rizanmohd030@gmail.com>
2026-01-11 16:19:45 +0530

    2nd project base

--- src/data/mainProjects.js
+++ src/data/mainProjects.js
@@ -1,4 +1,5 @@
 import RecipeFin from "../assets/images/RecipeFin.png";
+import GenAILaptop from "../assets/images/genai-laptop.png"; // your laptop mockup
 
 export const mainProjects = [
   {
@@ -8,7 +9,18 @@ export const mainProjects = [
     description2:
       "Its core differentiator is the food scanner, allowing users to scan ingredients using their camera and instantly receive relevant recipe suggestions.",
     live: "https://recipehunt.vercel.app/",
-    github:"https://github.com/Rizanmohd030/Recipe-Finder",
+    github: "https://github.com/Rizanmohd030/Recipe-Finder",
     heroImage: RecipeFin,
+  },
+
+  {
+    title: "GenAI Interview Assistant",
+    description1:
+      "An AI-powered platform that helps users build professional resumes and prepare for interviews effectively.",
+    description2:
+      "It generates personalized one-to-one interview questions based on role and experience to improve job readiness.",
+    live: "https://genai-resume.vercel.app/",
+    github: "vhttps://github.com/Rizanmohd030/GenAI-Resume-Assistant", // change if needed
+    heroImage: GenAILaptop,
   },
 ];
